3b815860ef523e452e9a529885348d2d535f7033
Leslie2186 <leslie.moraud@gmail.com>
2023-11-07 15:56:18 +0100

    Modifications des fichiers pour la récupération des données suite à la mise en place de la query limit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,9 @@
-import axios from "axios";
-import { useState } from "react";
 import "./App.css";
+import { useLoaderData } from "react-router-dom";
 import Cardheros from "./components/cardheros/Cardheros";
 
 function App() {
-  const [superheros, setSuperHeros] = useState([]);
-
-  const getAllSuperheros = () => {
-    axios
-      .get(`${import.meta.env.VITE_BACKEND_URL}/api/superheros`)
-      .then((response) => setSuperHeros(response.data[0]))
-      .catch((err) => console.error(err));
-  };
-  getAllSuperheros();
+  const superheros = useLoaderData();
 
   return (
     <div className="App">
--- frontend/src/components/cardheros/Cardheros.jsx
+++ frontend/src/components/cardheros/Cardheros.jsx
@@ -2,15 +2,11 @@ import "./Cardheros.css";
 import Cards from "../cards/Cards";
 
 function Cardheros({ superheros }) {
-  const count = window.innerWidth > 765 ? 8 : 4;
-
   return (
     <div className="containerCards">
-      {superheros
-        .filter((hero) => hero.id <= count)
-        .map((hero) => (
-          <Cards hero={hero} />
-        ))}
+      {superheros.map((hero) => (
+        <Cards key={hero.slug} hero={hero} />
+      ))}
     </div>
   );
 }
