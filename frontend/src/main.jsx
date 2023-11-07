b9d6292049d6159577981cc9d8aa036dfeb9c1fa
Leslie2186 <leslie.moraud@gmail.com>
2023-11-07 15:47:33 +0100

    Mise en place de la query de limit sur la taille de l'écran

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import axios from "axios";
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
 import App from "./App";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    loader: () => {
+      const count = window.innerWidth > 765 ? 8 : 4;
+      return axios
+        .get(
+          `${import.meta.env.VITE_BACKEND_URL}/api/superheros?limit=${count}`
+        )
+        .then((response) => response.data[0])
+        .catch((err) => console.error(err));
+    },
   },
 ]);
 
