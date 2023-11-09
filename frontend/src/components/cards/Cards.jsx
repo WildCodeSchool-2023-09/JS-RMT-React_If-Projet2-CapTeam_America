0f26bb0dbdec53b6e54a4c549a2e5676076f11cc
Leslie2186 <leslie.moraud@gmail.com>
2023-11-09 14:35:58 +0100

    Modification du logo de favoris en fonction de  mise ou retrait en favoris au clic

--- frontend/src/components/cards/Cards.jsx
+++ frontend/src/components/cards/Cards.jsx
@@ -1,9 +1,21 @@
 import "./Cards.css";
+import { useState } from "react";
 import elipseFist from "../../assets/ellipse-fist.png";
 import elipseFavoris from "../../assets/ellipse-favoris.png";
+import elipseFavorisOk from "../../assets/ellipse-favoris-ok.png";
 import elipsePoints from "../../assets/ellipse-points.png";
 
 function Cards({ hero }) {
+  const [isFavorite, setIsFavorite] = useState(`${elipseFavoris}`);
+
+  function goToFavorite() {
+    if (isFavorite === `${elipseFavoris}`) {
+      setIsFavorite(`${elipseFavorisOk}`);
+    } else {
+      setIsFavorite(`${elipseFavoris}`);
+    }
+  }
+
   return (
     <div className="cardHero">
       <img src={hero.image_sm} alt={hero.name} className="imgCard" />
@@ -12,8 +24,8 @@ function Cards({ hero }) {
         <p className="titleRace">{hero.race}</p>
       </div>
       <div className="containerButtons">
-        <button type="button">
-          <img src={elipseFavoris} alt="Ellipse Favoris" />
+        <button type="button" onClick={goToFavorite}>
+          <img src={isFavorite} alt="Ellipse Favoris" />
         </button>
         <button type="button" className="buttonFist">
           <img src={elipseFist} alt="Ellipse Fist" className="fist" />
