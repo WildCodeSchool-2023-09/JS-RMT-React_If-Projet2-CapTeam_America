6c90e4a677979c8b56704904621cd3d6968f19a6
MAXIME GAEL <maximejordan.gael@gmail.com>
2023-11-10 13:46:06 +0100

    ajout de la partie navbar en version mobile

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,19 +4,11 @@ import axios from "axios";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App";
 import CompleteCard from "./pages/CompleteCard";
+import Cardheros from "./components/cardheros/Cardheros";
 
 const router = createBrowserRouter([
   {
     element: <App />,
-    loader: () => {
-      const count = window.innerWidth > 765 ? 8 : 4;
-      return axios
-        .get(
-          `${import.meta.env.VITE_BACKEND_URL}/api/superheros?limit=${count}`
-        )
-        .then((response) => response.data)
-        .catch((err) => console.error(err));
-    },
     children: [
       {
         path: "/",
