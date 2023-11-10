5cc63ce4a36044fb4b9713092596b383911d80b1
Defreitasnicolas <defreitasnicolas@gmail.com>
2023-11-10 11:09:36 +0100

    supression des commentaires

--- frontend/src/components/navbarBottom/NavbarBottom.jsx
+++ frontend/src/components/navbarBottom/NavbarBottom.jsx
@@ -13,24 +13,6 @@ function NavbarBottom() {
         width={30}
         height={30}
       />
-
-      {/* <button type="button" className="favorite">
-        <Link to="/Favoris">
-          <img src={notFavorite} alt="favorite" height={27} width={27} />
-        </Link>
-      </button>
-
-      <button type="button" className="contact">
-        <Link to="/Contact">
-          <img src={contact} alt="contact" width={40} height={40} />
-        </Link>
-      </button>
-
-      <button type="button" className="versus">
-        <Link to="/Versus">
-          <img src={versus} alt="versus" width={45} height={45} />
-        </Link>
-      </button> */}
     </div>
   );
 }
