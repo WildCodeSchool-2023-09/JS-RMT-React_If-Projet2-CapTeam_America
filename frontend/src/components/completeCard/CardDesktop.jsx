36528a9818a3dcda644bbe3a217844a4bd0c4e09
MAXIME GAEL <maximejordan.gael@gmail.com>
2023-11-14 15:46:02 +0100

    utilisation des composants plutôt que de détailler les UL et LI

--- frontend/src/components/completeCard/CardDesktop.jsx
+++ frontend/src/components/completeCard/CardDesktop.jsx
@@ -1,12 +1,9 @@
 import React from "react";
-import "./CardDesktop.css";
 import { useLoaderData } from "react-router-dom";
-import intelligence from "../../assets/intelligence.png";
-import strength from "../../assets/strength.png";
-import speed from "../../assets/speed.png";
-import durability from "../../assets/durability.png";
-import power from "../../assets/power.png";
-import combat from "../../assets/combat.png";
+import "./CardDesktop.css";
+import "./CompleteCard.css";
+import Stats from "./Stats";
+import Identity from "./Identity";
 
 function CardDesktop() {
   const oneSuperHero = useLoaderData();
@@ -20,63 +17,14 @@ function CardDesktop() {
         />
         <div className="superHeroIdentity">
           <h1 className="superHeroName">{oneSuperHero.name}</h1>
-          <h1 className="superHeroRace">{oneSuperHero.race}</h1>
+          <h2 className="superHeroRace">{oneSuperHero.race}</h2>
         </div>
       </div>
       <div className="containerIdentity">
-        <ul>
-          <h2>Profile</h2>
-          <li>Gender: {oneSuperHero.gender}</li>
-          <li>Eye Color: {oneSuperHero.eyecolor}</li>
-          <li>Hair Color: {oneSuperHero.haircolor}</li>
-          <li>Full Name: {oneSuperHero.fullname}</li>
-          <li>Place Of Birth: {oneSuperHero.place_of_birth}</li>
-          <li>First Appearance: {oneSuperHero.first_appearance}</li>
-          <li>Work: {oneSuperHero.work}</li>
-        </ul>
+        <Identity oneSuperHero={oneSuperHero} />
       </div>
       <div className="containerStats">
-        <ul>
-          <h2>Stats</h2>
-          <div className="icons">
-            <li>
-              <img
-                src={intelligence}
-                alt="intelligence icon"
-                className="intelligenceIcon"
-              />
-              {oneSuperHero.intelligence}
-            </li>
-            <li>
-              <img
-                src={strength}
-                alt="strength icon"
-                className="strengthIcon"
-              />
-              {oneSuperHero.strength}
-            </li>
-            <li>
-              <img src={speed} alt="speed icon" className="speedIcon" />
-              {oneSuperHero.speed}
-            </li>
-            <li>
-              <img
-                src={durability}
-                alt="durability icon"
-                className="durabilityIcon"
-              />
-              {oneSuperHero.durability}
-            </li>
-            <li>
-              <img src={power} alt="power icon" className="powerIcon" />
-              {oneSuperHero.power}
-            </li>
-            <li>
-              <img src={combat} alt="combat icon" className="combatIcon" />
-              {oneSuperHero.combat}
-            </li>
-          </div>
-        </ul>
+        <Stats oneSuperHero={oneSuperHero} />
       </div>
     </div>
   );
