314ab5ff298c670a2dfe6e613c7b533d8cd403e1
Leslie2186 <leslie.moraud@gmail.com>
2023-11-16 16:26:44 +0100

    Modification du component favoris pour déplacer la logique

--- frontend/src/components/favoris/Favoris.jsx
+++ frontend/src/components/favoris/Favoris.jsx
@@ -5,25 +5,21 @@ import elipseFavorisOk from "../../assets/ellipse-favoris-ok.png";
 import elipsePoints from "../../assets/ellipse-points.png";
 
 function Favoris() {
-  const { goFavorite } = useContext(SuperheroContext);
+  const { favories, handleFavories } = useContext(SuperheroContext);
 
   return (
     <div>
       <h1 className="titleFavoris">My superhero favourites</h1>
       <div className="containerCards">
-        {goFavorite.map((herofav) => (
-          <div key={herofav.id} className="cardHero">
-            <img
-              src={herofav.image_sm}
-              alt={herofav.name}
-              className="imgCard"
-            />
+        {favories.map((hero) => (
+          <div key={hero.id} className="cardHero">
+            <img src={hero.image_sm} alt={hero.name} className="imgCard" />
             <div className="containerNameRace">
-              <p className="titleName">{herofav.name.toUpperCase()}</p>
-              <p className="titleRace">{herofav.race}</p>
+              <p className="titleName">{hero.name.toUpperCase()}</p>
+              <p className="titleRace">{hero.race}</p>
             </div>
             <div className="containerButtons buttonNav">
-              <button type="button">
+              <button type="button" onClick={() => handleFavories(hero)}>
                 <img src={elipseFavorisOk} alt="Ellipse Favoris" />
               </button>
               <button type="button">
